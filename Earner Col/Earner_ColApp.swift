023a4b40b2e373848ecfd788b902c8a756a3c1b1
Alexander <alexrubino05@gmail.com>
2023-01-30 18:35:16 +0100

    Initial Commit

---
+++ Earner Col/Earner_ColApp.swift
@@ -0,0 +1,17 @@
+//
+//  Earner_ColApp.swift
+//  Earner Col
+//
+//  Created by Alexander Rubino on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Earner_ColApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
